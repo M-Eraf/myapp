dd8006a353cfef9da2b0cbb153b6cc67fbbd51e7
m-eraf <eraf9120@gmail.com>
2022-06-14 02:06:36 +0530

    edit

--- routes/index.js
+++ routes/index.js
@@ -3,43 +3,32 @@ var router = express.Router();
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "data"
+  host: "easylearning.guru",
+  user: "kcc_student",
+  password: "Kccitm.edu.in1",
+  database: "kccStudent"
 });
+
 
 router.get('/', function(req, res, next) {
     con.connect(function(err) {
         if (err) console.log("err");
-        console.log("Connected to mySQL!");
+        console.log("Connected to mySQL server!");
       });
     res.render('index')
 });
 
 router.post('/about', function(req, res, next) {
     console.log(req.body)
-    var sql = "INSERT INTO `form_details` (`name`, `country`, `age`) \
-    VALUES ('"+req.body.name+"', '"+req.body.country+"', '"+req.body.age+"');"
+    var sql = "INSERT INTO `table_name` (`name`, `country`,`age`) \
+    VALUES ('"+req.body.name+"', '"+req.body.country+"','"+req.body.age+"');"
     console.log(sql)
    con.connect()
       con.query(sql, function (err, result) {
         if (err) throw err;
         console.log(result);
-
       });
-//       var sql1 = "select * from `form_details`"
-//       con.query(sql1, function (err, result) {
-//         if (err) throw err;
-//         console.log(result);
-//       });
 res.json({"Name":req.body})
 });
-
-// router.get('/where', function(req, res, next) {
-//   //   res.send('eraf');
-//     console.log(req)
-//     res.json({name:"eraf"})
-//   });
 
 module.exports = router;
